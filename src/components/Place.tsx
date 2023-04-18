97c4885113ba06651e54338d6f9173ad3b14b22e
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-04-17 23:52:07 -0500

    feat: Create Place component to display location data

---
+++ src/components/Place.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import Search from "./Search";
+
+const Place = () => {
+  return (
+    <div className="p-5 gap-4 flex flex-col lg:p-10 lg:justify-between lg:flex-row w-full h-[25rem] rounded-[2.5rem] bg-light">
+      <div className="lg:px-16 grid place-content-center">
+        <h1 className="text-4xl text-dark font-bold">Guayaquil</h1>
+      </div>
+      <Search/>
+    </div>
+  );
+};
+
+export default Place;
