272da7757070b88b0c062b58ce624405809ab467
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-04-18 19:01:25 -0500

    feat: add zustand for state management

---
+++ src/store/weatherStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+export const useWeatherStore = create<WeatherState>((set)=>({
+    lat: 0,
+    lon: 0,
+    place: {} as Location,
+    weather: {} as WeatherResponse,
+    image: {} as Image,
+    setLocation: (lat , lon) => set({lat, lon}),
+    setPlace: (place) => set({place}),
+    setWeather: (weather) => set({weather}),
+    setImage: (image) => set({image})
+}))
